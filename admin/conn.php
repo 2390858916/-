ff2e17e33b215605d96e6b961f316ccb8d822eb2
2390858916 <43715007+2390858916@users.noreply.github.com>
2021-01-21 12:28:13 +0800

    Add files via upload

---
+++ admin/conn.php
@@ -0,0 +1,9 @@
+<?php
+	$conn=mysqli_connect("localhost","root","root","cms");
+	mysqli_query($conn,"set name utf8");
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
